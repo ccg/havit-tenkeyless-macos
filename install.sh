45b6788c84ef8e8396de24f9df79d2d2f7893047
Chad Glendenin <chad.glendenin@rstudio.com>
2018-03-08 16:25:34 -0600

    Add install script (untested)

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+KARABINER_MOD_DIR=$HOME/.config/karabiner/assets/complex_modifications/
+
+mkdir -p $KARABINER_MOD_DIR
+ln -s PrintScreenMissionControl.json $KARABINER_MOD_DIR
+ln -s OptPageUpDownVolumeUpDown.json $KARABINER_MOD_DIR
